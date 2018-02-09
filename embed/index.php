12d40bbd88496a0d2e7255651609b72b9a75141e
Khoerodin <khoerodin@live.com>
2018-02-09 15:15:37 +0700

    fix embed youtube, vimeo, twitter, and image

--- embed/index.php
+++ embed/index.php
@@ -13,16 +13,18 @@ class Grab {
     }
 
     $embed = Embed::create($url);
-    $type = $embed->type;
+    $provider = strtolower($embed->providerName);
     if ($type !== 'photo' && $type !== 'link') {
-      return $this->$type($embed);
+      return $this->$provider($embed);
     }
 
     return false;
   }
 
-  function video($embed)
+  function twitter($embed)
   {
+    $id = end(explode('/', $embed->url));
+
     echo "<!DOCTYPE html>\n";
     echo "<html>\n";
     echo "  <head>\n";
@@ -31,18 +33,94 @@ class Grab {
     echo "    <meta name=\"format-detection\" content=\"telephone=no\" />\n";
     echo "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />\n";
     echo "    <meta name=\"MobileOptimized\" content=\"320\" />\n";
-    echo "    <style>body{margin:0;padding:0}html,body,iframe,video,object,embed{height:100%;width:100%;overflow:hidden;}</style>\n";
+    echo "    <meta name=\"twitter:widgets:csp\" content=\"on\">\n";
+    echo "    <script src=\"//platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>\n";
+    echo "    <style>body{margin:0;padding:0}.twitter-tweet{margin:0 auto!important}</style>\n";
     echo "  </head>\n";
-    echo "  <body>\n";
-    echo "    " . $embed->code;
+    echo "  <body onresize=\"resize()\">\n";
+    echo "    <div id=\"tweet\"></div>\n";
+    echo "    <script>\n";
+    echo "      var _htmlNode = document.getElementsByTagName('html')[0];\n";
+    echo "      var _maxHeight = 0;\n";
+    echo "      function resize() {\n";
+    echo "        if (parent && parent._resizeIframe) {\n";
+    echo "          var width  = _htmlNode.offsetWidth;\n";
+    echo "          var height = _maxHeight ? Math.min(_maxHeight, _htmlNode.offsetHeight) : _htmlNode.offsetHeight;\n";
+    echo "          parent._resizeIframe(window, width, height);\n";
+    echo "        }\n";
+    echo "      };\n";
+    echo "      var frames = document.getElementsByTagName('iframe');\n";
+    echo "      for (var i = 0; i < frames.length; (frames[i].onload = resize), i++);\n";
+    echo "      window.twttr&&twttr.widgets&&twttr.widgets.createTweet(\"". $id ."\",document.getElementById(\"tweet\"), resize);\n";
+    echo "    </script>\n";
     echo "  </body>\n";
     echo "</html>";
   }
 
-  function rich($embed)
+  function youtube($embed)
   {
-    echo "<script src=\"/js/iframeResizer.contentWindow.min.js\"></script>";
-    echo "<center>" . $embed->code . "</center>";
+    $id = end(explode('?v=', $embed->url));
+
+    echo "<!DOCTYPE html>\n";
+    echo "<html>\n";
+    echo "  <head>\n";
+    echo "    <meta charset=\"utf-8\">\n";
+    echo "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no\" />\n";
+    echo "    <meta name=\"format-detection\" content=\"telephone=no\" />\n";
+    echo "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />\n";
+    echo "    <meta name=\"MobileOptimized\" content=\"320\" />\n";
+    echo "    <style>body{margin:0;padding:0}html,body,iframe{height:100%}</style>\n";
+    echo "  </head>\n";
+    echo "  <body onresize=\"resize()\">\n";
+    echo "    <iframe width=\"100%\" height=\"100%\" src=\"//www.youtube.com/embed/". $id ."?rel=0&showinfo=0\" frameborder=\"0\" allowtransparency=\"true\" allowfullscreen=\"true\"></iframe>\n";
+    echo "    <script>\n";
+    echo "      var _htmlNode = document.getElementsByTagName('html')[0];\n";
+    echo "      var _maxHeight = 0;\n";
+    echo "      function resize() {\n";
+    echo "        if (parent && parent._resizeIframe) {\n";
+    echo "          var width  = _htmlNode.offsetWidth;\n";
+    echo "          var height = _maxHeight ? Math.min(_maxHeight, _htmlNode.offsetHeight) : _htmlNode.offsetHeight;\n";
+    echo "          parent._resizeIframe(window, width, height);\n";
+    echo "        }\n";
+    echo "      };\n";
+    echo "      var frames = document.getElementsByTagName('iframe');\n";
+    echo "      for (var i = 0; i < frames.length; (frames[i].onload = resize), i++);\n";
+    echo "    </script>\n";
+    echo "  </body>\n";
+    echo "</html>";
+  }
+
+  function vimeo($embed)
+  {
+    $id = end(explode('/', $embed->url));
+
+    echo "<!DOCTYPE html>\n";
+    echo "<html>\n";
+    echo "  <head>\n";
+    echo "    <meta charset=\"utf-8\">\n";
+    echo "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no\" />\n";
+    echo "    <meta name=\"format-detection\" content=\"telephone=no\" />\n";
+    echo "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />\n";
+    echo "    <meta name=\"MobileOptimized\" content=\"320\" />\n";
+    echo "    <style>body{margin:0;padding:0}html,body,iframe{height:100%}</style>\n";
+    echo "  </head>\n";
+    echo "  <body onresize=\"resize()\">\n";
+    echo "    <iframe width=\"100%\" height=\"100%\" src=\"//player.vimeo.com/video/". $id ."/\" frameborder=\"0\" allowtransparency=\"true\" allowfullscreen=\"true\"></iframe>\n";
+    echo "    <script>\n";
+    echo "      var _htmlNode = document.getElementsByTagName('html')[0];\n";
+    echo "      var _maxHeight = 0;\n";
+    echo "      function resize() {\n";
+    echo "        if (parent && parent._resizeIframe) {\n";
+    echo "          var width  = _htmlNode.offsetWidth;\n";
+    echo "          var height = _maxHeight ? Math.min(_maxHeight, _htmlNode.offsetHeight) : _htmlNode.offsetHeight;\n";
+    echo "          parent._resizeIframe(window, width, height);\n";
+    echo "        }\n";
+    echo "      };\n";
+    echo "      var frames = document.getElementsByTagName('iframe');\n";
+    echo "      for (var i = 0; i < frames.length; (frames[i].onload = resize), i++);\n";
+    echo "    </script>\n";
+    echo "  </body>\n";
+    echo "</html>";
   }
 }
 
--- js/script.js
+++ js/script.js
@@ -1,73 +1,108 @@
-var isSelectionInsideElement = function (tagName) {
-  var sel, containerNode
-  tagName = tagName.toUpperCase()
-  if (window.getSelection) {
-    sel = window.getSelection()
-    if (sel.rangeCount > 0) {
-      containerNode = sel.getRangeAt(0).commonAncestorContainer
+class Utility {
+  getFigureTypeByUrl (url) {
+    var figure = false
+    if (url.match(/^(https?):\/\/(www\.)?youtube\.com\/watch.*v=([a-zA-Z0-9_-]+)/i) || url.match(/^(https?):\/\/(www\.)?youtu\.be\/([a-zA-Z0-9_-]+)/i)) {
+      figure = "iframe"
     }
-  } else if ((sel = document.selection) && sel.type != "Control") {
-    containerNode = sel.createRange().parentElement()
-  }
-  while (containerNode) {
-    if (containerNode.nodeType == 1 && containerNode.tagName == tagName) {
-      return true
-    }
-    containerNode = containerNode.parentNode
-  }
-  return false
-},
 
-  slicedToArray = function () {
-    function t(t, e) {
-      var o = [],
-        l = !0,
-        i = !1,
-        r = void 0;
-      try {
-        for (var a, n = t[Symbol.iterator](); !(l = (a = n.next()).done) && (o.push(a.value), !e || o.length !== e); l = !0);
-      } catch (s) {
-        i = !0, r = s
-      } finally {
-        try {
-          !l && n["return"] && n["return"]()
-        } finally {
-          if (i) throw r
-        }
+    if (url.match(/^(https?):\/\/(www\.)?vimeo\.com\/(\d+)/i)) {
+      figure = "iframe"
+    }
+
+    if (url.match(/^(https?):\/\/(www\.|mobile\.)?twitter\.com\/(.+)\/status\/(\d+)/i)) {
+      figure = "iframe"
+    }
+
+    if (url.match(/^data:(image\/gif|image\/jpe?g|image\/png);base64,(.*)$/) || url.match(/\.(jpe?g|png|gif)$/i)) {
+      figure = "image"
+    }
+
+    return figure
+  }
+
+  isSelectionInsideElement (tagName) {
+    var sel, containerNode
+    tagName = tagName.toUpperCase()
+    if (window.getSelection) {
+      sel = window.getSelection()
+      if (sel.rangeCount > 0) {
+        containerNode = sel.getRangeAt(0).commonAncestorContainer
       }
-      return o
+    } else if ((sel = document.selection) && sel.type != "Control") {
+      containerNode = sel.createRange().parentElement()
     }
-    return function (e, o) {
-      if (Array.isArray(e)) return e
-      if (Symbol.iterator in Object(e)) return t(e, o)
-      throw new TypeError("Invalid attempt to destructure non-iterable instance")
+    while (containerNode) {
+      if (containerNode.nodeType == 1 && containerNode.tagName == tagName) {
+        return true
+      }
+      containerNode = containerNode.parentNode
     }
-  }(),
+    return false
+  }
 
-  removePlaceholder = function () {
+  removePlaceholder () {
     $('.embed-area').removeAttr('data-placeholder').removeClass('embed-area')
-  },
+  }
 
-  readUrl = function (url) {
-    var embedData = {
-      src: url
+  readUrl (url) {
+    if (this.getFigureTypeByUrl(url)) {
+      this.createFigure({ src: url })
+      document.querySelector('.url-area').remove() // tempat paste url
+      document.querySelector('.url-area').removeAttribute('class') // tempat kursor bawah figure
+      return false;
     }
-    createFigure(embedData)
-    document.querySelector('.url-area').remove() // tempat paste url
-    document.querySelector('.url-area').removeAttribute('class') // tempat kursor bawah figure
 
-    return false;
-  },
+    return true
+  }
 
-  createFigure = function (embedData) {
+  createFigure (embedData) {
     var range = quill.getSelection(true)
     quill.insertEmbed(range.index, 'figure', embedData, Quill.sources.USER)
     quill.setSelection(range.index + 2, Quill.sources.SILENT)
-  },
+  }
+}
+var Utilities = new Utility()
 
-  fixHeight = function () {
-    iFrameResize([{log:false}], 'iframe.rich')
-  };
+var slicedToArray = function () {
+  function t(t, e) {
+    var o = [],
+      l = !0,
+      i = !1,
+      r = void 0;
+    try {
+      for (var a, n = t[Symbol.iterator](); !(l = (a = n.next()).done) && (o.push(a.value), !e || o.length !== e); l = !0);
+    } catch (s) {
+      i = !0, r = s
+    } finally {
+      try {
+        !l && n["return"] && n["return"]()
+      } finally {
+        if (i) throw r
+      }
+    }
+    return o
+  }
+  return function (e, o) {
+    if (Array.isArray(e)) return e
+    if (Symbol.iterator in Object(e)) return t(e, o)
+    throw new TypeError("Invalid attempt to destructure non-iterable instance")
+  }
+}()
+
+function _resizeIframe(t, e, o) {
+  $("iframe").map(function() {
+      var l = null;
+      try {
+          l = this.contentWindow
+      } catch (i) {}
+      if (l && l == t) {
+          var r = o / e;
+          this.setAttribute("width", "680"),
+          this.setAttribute("height", Math.round(680 * r) + ""),
+          this.parentNode && this.parentNode.classList.contains("iframe-helper") && (this.parentNode.style.paddingTop = 100 * r + "%")
+      }
+  })
+}
 
 var Block = Quill.import('blots/block')
 var BlockEmbed = Quill.import('blots/block/embed')
@@ -83,54 +118,51 @@ Quill.register(DividerBlot)
 class FigureBlot extends BlockEmbed {
   static create(embedData) {
     let figure = super.create();
+    figure.dataset.src = embedData.src
     figure.setAttribute('contenteditable', false)
-    figure.dataset.type = embedData.type;
-    figure.dataset.src = embedData.src;
 
     var figcaption = document.createElement('figcaption')
     figcaption.setAttribute('contenteditable', true)
     figcaption.setAttribute('data-placeholder', 'Judul (opsional)')
     figcaption.innerText = embedData.caption ? embedData.caption : ''
 
-    // var figureType = embedData.type
-    // this[figureType](figure)
-    this.rich(figure)
+    var figureWrapper = document.createElement('div')
+    figureWrapper.setAttribute('class', 'figure-wrapper')
+    figure.appendChild(figureWrapper)
+
+    var figureType = Utilities.getFigureTypeByUrl(embedData.src)
+    this[figureType](figureWrapper, embedData.src)
 
     figure.appendChild(figcaption)
     return figure;
   }
 
-  static rich(figure) {
+  static iframe(figureWrapper, src) {
+    var iframeWrapper = document.createElement('div')
+    iframeWrapper.setAttribute('class', 'iframe-wrapper')
+
+    var iframeHelper = document.createElement('div')
+    iframeHelper.setAttribute('class', 'iframe-helper')
+    iframeHelper.style.paddingTop = '56.25%'
+
     var iframe = document.createElement('iframe')
-    iframe.setAttribute('src', this.embedUrl() + figure.dataset.src)
-    iframe.setAttribute('class', 'rich')
-    iframe.setAttribute('frameborder', '0')
+    iframe.setAttribute('src', this.embedUrl() + src)
+    iframe.setAttribute('frameborder', 0)
     iframe.setAttribute('allowtransparency', true)
     iframe.setAttribute('allowfullscreen', true)
     iframe.setAttribute('scrolling', 'no')
-    iframe.setAttribute('onload', 'fixHeight()')
+    iframe.setAttribute("width", "680")
+    iframe.setAttribute("height", "auto")
 
-    return figure.appendChild(iframe)
+    figureWrapper.appendChild(iframeWrapper)
+    iframeWrapper.appendChild(iframeHelper)
+    iframeHelper.appendChild(iframe)
   }
 
-  static video(figure) {
-    var wrapper = document.createElement('div')
-    wrapper.setAttribute('class', 'video-wrapper')
-
-    var iframe = document.createElement('iframe')
-    iframe.setAttribute('src', this.embedUrl() + figure.dataset.src)
-    iframe.setAttribute('frameborder', '0')
-    iframe.setAttribute('allowtransparency', true)
-    iframe.setAttribute('allowfullscreen', true)
-    iframe.setAttribute('scrolling', 'no')
-
-    wrapper.appendChild(iframe)
-
-    return figure.appendChild(wrapper)
-  }
-
-  static image() {
-
+  static image(figureWrapper, src) {
+    var img = document.createElement('img')
+    img.setAttribute('src', src)
+    figureWrapper.appendChild(img)
   }
 
   static embedUrl() {
@@ -139,7 +171,6 @@ class FigureBlot extends BlockEmbed {
 
   static value(figure) {
     return {
-      type: figure.dataset.type,
       src: figure.dataset.src,
       caption: figure.getElementsByTagName('figcaption')[0].innerText
     }
@@ -167,12 +198,12 @@ var quill = new Quill('#editor-container', {
 
 quill.addContainer($("#sidebar-controls").get(0))
 quill.on(Quill.events.EDITOR_CHANGE, function (eventType, range) {
-  removePlaceholder()
+  Utilities.removePlaceholder()
   if (eventType !== Quill.events.SELECTION_CHANGE) return
   if (range == null) return
   if (range.length === 0) {
     var [block, offset] = quill.scroll.descendant(Block, range.index)
-    if (block != null && block.domNode.firstChild instanceof HTMLBRElement && !isSelectionInsideElement('li')) {
+    if (block != null && block.domNode.firstChild instanceof HTMLBRElement && !Utilities.isSelectionInsideElement('li')) {
       var lineBounds = quill.getBounds(range)
       $('#sidebar-controls').show().css({
         left: lineBounds.left - 120,
@@ -185,8 +216,8 @@ quill.on(Quill.events.EDITOR_CHANGE, function (eventType, range) {
     $('#sidebar-controls').fadeOut()
   }
 
-  var delta = quill.getContents()
-  console.log('Delta:', JSON.stringify(delta.ops))
+  // var delta = quill.getContents()
+  // console.log('Delta:', JSON.stringify(delta.ops))
 });
 
 document.getElementById('divider-button').addEventListener('click', function () {
@@ -207,32 +238,28 @@ document.getElementById('embed-button').addEventListener('click', function () {
   }
 });
 
-document.getElementById('image-button').addEventListener('click', function () {
-  var inputFile = quill.container.querySelector("input[type=file][data-status=ready]")
-  if (!inputFile) {
-    inputFile = document.createElement("input")
-    inputFile.setAttribute("type", "file")
-    inputFile.setAttribute("accept", "image/gif, image/jpeg, image/jpg, image/png")
-    quill.container.appendChild(inputFile)
-    inputFile.click()
-    inputFile.addEventListener("change", function(e) {
-      var loadingImage = loadImage(
-        e.target.files[0],
-        function (img) {
-          createFigure({src: img.getAttribute('src')})
-        }
-      )
+var inputFile = document.createElement("input")
+inputFile.setAttribute("type", "file")
+inputFile.setAttribute("accept", "image/gif, image/jpeg, image/jpg, image/png")
+inputFile.setAttribute("style", "display:none")
 
-      if (!loadingImage) {
-        // Alternative code ...
-      }
-    })
+quill.container.appendChild(inputFile)
+
+inputFile.addEventListener("change", function (e) {
+  var reader = new FileReader()
+  reader.onload = function() {
+    var base64data = reader.result
+    Utilities.createFigure({ src: base64data })
   }
-});
+  reader.readAsDataURL(e.target.files[0])
+})
+
+document.getElementById('image-button').addEventListener('click', function () {
+  inputFile.click()
+})
 
 quill.keyboard.addBinding({ key: Keyboard.keys.ENTER }, function (t, e) {
-  var a = quill,
-    o = a.scroll.line(t.index),
+  var o = quill.scroll.line(t.index),
     l = slicedToArray(o, 2),
     i = l[0],
     r = l[1];
@@ -241,7 +268,7 @@ quill.keyboard.addBinding({ key: Keyboard.keys.ENTER }, function (t, e) {
       s = n.substr(0, r),
       u = void 0;
     if (u = s.match(/(^|\s)(https?:\/\/\S+)$/)) {
-      return readUrl(u[2])
+      return Utilities.readUrl(u[2])
     } else {
       return true
     }
@@ -251,8 +278,8 @@ quill.keyboard.addBinding({ key: Keyboard.keys.ENTER }, function (t, e) {
 quill.keyboard.bindings[Keyboard.keys.ENTER].unshift(quill.keyboard.bindings[Keyboard.keys.ENTER].pop())
 
 // set contents
-// var contents = [{ "insert": "Toshi Omagari’s updated version opens up the typeface - version opens up the typeface" }, { "attributes": { "header": 1 }, "insert": "\n" }, { "insert": "The decorative blackletter – " }, { "attributes": { "link": "#" }, "insert": "which offers a stark contrast with" }, { "insert": " the other four typefaces included in the Wolpe Collection – drew on Berthold Wolpe’s time working in a metal foundry. As a result, it adopts the kinds of angular forms that would have been more easily chiselled into metal – appearing slightly less intricate than fellow blackletter faces.\nAn early specimen sheet, published in 1938, said the typeface was intending to cause a stir among “horizon-scanning advertisers”, however the design quickly fell into obscurity, particularly as a result of blackletter’s connotations." }, { "attributes": { "blockquote": true }, "insert": "\n" }, { "insert": "An early specimen sheet" }, { "attributes": { "header": 2 }, "insert": "\n" }, { "insert": "List Pertama" }, { "attributes": { "list": "bullet" }, "insert": "\n" }, { "insert": "List Kedua" }, { "attributes": { "list": "bullet" }, "insert": "\n" }, { "insert": "List Ketiga" }, { "attributes": { "list": "bullet" }, "insert": "\n" }, { "insert": "List Keempat" }, { "attributes": { "list": "bullet" }, "insert": "\n" }, { "insert": "List Kelima" }, { "attributes": { "list": "bullet" }, "insert": "\n" }, { "insert": "An early specimen sheet" }, { "attributes": { "header": 2 }, "insert": "\n" }, { "insert": "List Pertama" }, { "attributes": { "list": "ordered" }, "insert": "\n" }, { "insert": "List Kedua" }, { "attributes": { "list": "ordered" }, "insert": "\n" }, { "insert": "List Ketiga" }, { "attributes": { "list": "ordered" }, "insert": "\n" }, { "insert": "List Keempat" }, { "attributes": { "list": "ordered" }, "insert": "\n" }, { "insert": "List Kelima" }, { "attributes": { "list": "ordered" }, "insert": "\n" }, { "insert": "There were reportedly only two sets of matrices ever made of Sachsenwald, which otherwise has lived on only through original drawings held by the Monotype archive. \n" }, { "insert": { "divider": true } }, { "insert": "As you’d expect, examples of it being used are limited, however it can be seen in a rare edition of the Rubaiyat of Omar Khayyam, published by Fanfare Press in the 1940s.\n" }, {"insert": {"figure": {"type": "video", "src": "https://vimeo.com/195304295", "caption": "Ini Judul/Caption Video"} } }, { "insert": "Toshi Omagari’s updated version opens up the typeface for experimentation by today’s designers, and also introduces elements that make it easier for today’s purposes – including a romanized version of the distinctive traditional German X originally used in Sachsenwald.\nGet the Sachsenwald typeface" }, { "attributes": { "header": 2 }, "insert": "\n" }, { "insert": "View the web specimen for Sachsenwald and The Wolpe Collection. All five families are included in Monotype Library Subscription. Get unlimited access to over 10,000 fonts for $9.99/month. Try it now for free.\nHeading 1" }, { "attributes": { "header": 1 }, "insert": "\n" }, { "insert": "Heading 2" }, { "attributes": { "header": 2 }, "insert": "\n" }, { "insert": "Heading 3" }, { "attributes": { "header": 3 }, "insert": "\n" }, { "insert": "Heading 4" }, { "attributes": { "header": 4 }, "insert": "\n" }, { "insert": "Heading 5" }, { "attributes": { "header": 5 }, "insert": "\n" }, { "insert": "Heading 6" }, { "attributes": { "header": 6 }, "insert": "\n" }]
-var contents = [{"insert": "Lalalalalalal"},{"insert": {"figure": {"type": "video", "src": "https://vimeo.com/195304295", "caption": "Ini Judul/Caption Video"}}}]
+var contents = [{ "insert": "Toshi Omagari’s updated version opens up the typeface - version opens up the typeface" }, { "attributes": { "header": 1 }, "insert": "\n" }, { "insert": "The decorative blackletter – " }, { "attributes": { "link": "#" }, "insert": "which offers a stark contrast with" }, { "insert": " the other four typefaces included in the Wolpe Collection – drew on Berthold Wolpe’s time working in a metal foundry. As a result, it adopts the kinds of angular forms that would have been more easily chiselled into metal – appearing slightly less intricate than fellow blackletter faces.\nAn early specimen sheet, published in 1938, said the typeface was intending to cause a stir among “horizon-scanning advertisers”, however the design quickly fell into obscurity, particularly as a result of blackletter’s connotations." }, { "attributes": { "blockquote": true }, "insert": "\n" }, { "insert": "An early specimen sheet" }, { "attributes": { "header": 2 }, "insert": "\n" }, { "insert": "List Pertama" }, { "attributes": { "list": "bullet" }, "insert": "\n" }, { "insert": "List Kedua" }, { "attributes": { "list": "bullet" }, "insert": "\n" }, { "insert": "List Ketiga" }, { "attributes": { "list": "bullet" }, "insert": "\n" }, { "insert": "List Keempat" }, { "attributes": { "list": "bullet" }, "insert": "\n" }, { "insert": "List Kelima" }, { "attributes": { "list": "bullet" }, "insert": "\n" }, { "insert": "An early specimen sheet" }, { "attributes": { "header": 2 }, "insert": "\n" }, { "insert": "List Pertama" }, { "attributes": { "list": "ordered" }, "insert": "\n" }, { "insert": "List Kedua" }, { "attributes": { "list": "ordered" }, "insert": "\n" }, { "insert": "List Ketiga" }, { "attributes": { "list": "ordered" }, "insert": "\n" }, { "insert": "List Keempat" }, { "attributes": { "list": "ordered" }, "insert": "\n" }, { "insert": "List Kelima" }, { "attributes": { "list": "ordered" }, "insert": "\n" }, { "insert": "There were reportedly only two sets of matrices ever made of Sachsenwald, which otherwise has lived on only through original drawings held by the Monotype archive. \n" }, { "insert": { "divider": true } }, { "insert": "As you’d expect, examples of it being used are limited, however it can be seen in a rare edition of the Rubaiyat of Omar Khayyam, published by Fanfare Press in the 1940s.\n" }, {"insert": {"figure": {"src": "https://vimeo.com/195304295", "caption": "Ini Judul/Caption Video"} } }, { "insert": "Toshi Omagari’s updated version opens up the typeface for experimentation by today’s designers, and also introduces elements that make it easier for today’s purposes – including a romanized version of the distinctive traditional German X originally used in Sachsenwald.\nGet the Sachsenwald typeface" }, { "attributes": { "header": 2 }, "insert": "\n" }, { "insert": "View the web specimen for Sachsenwald and The Wolpe Collection. All five families are included in Monotype Library Subscription. Get unlimited access to over 10,000 fonts for $9.99/month. Try it now for free.\nHeading 1" }, { "attributes": { "header": 1 }, "insert": "\n" }, { "insert": "Heading 2" }, { "attributes": { "header": 2 }, "insert": "\n" }, { "insert": "Heading 3" }, { "attributes": { "header": 3 }, "insert": "\n" }, { "insert": "Heading 4" }, { "attributes": { "header": 4 }, "insert": "\n" }, { "insert": "Heading 5" }, { "attributes": { "header": 5 }, "insert": "\n" }, { "insert": "Heading 6" }, { "attributes": { "header": 6 }, "insert": "\n" }]
+// var contents = [{ "insert": "Lalalalalalal" }, { "insert": { "figure": { "type": "video", "src": "https://vimeo.com/195304295", "caption": "Ini Judul/Caption Video" } } }]
 // var contents = [{ "insert": "Lalalalalalal" }, { "insert": { "figure": { "type": "rich", "src": "https://twitter.com/CGV_ID/status/924530299735752705", "caption": "Ini Judul/Caption Video" } } }]
 // var contents = [{ "insert": "Lalalalalalal" }, { "insert": { "figure": { "type": "rich", "src": "https://gist.github.com/khoerodin/fb7ab7d7c892243622c6b8dc6f9ad5dc", "caption": "Ini Judul/Caption Video" } } }]
 // var contents = [{ "insert": "Lalalalalalal" }, { "insert": { "figure": { "type": "rich", "src": "https://www.facebook.com/mamaeh.edy/posts/2050682998513142", "caption": "Ini Judul/Caption Video" } } }]
