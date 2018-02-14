ea76130a34ab028fdfa23bdb8986a139502d6317
Khoerodin <khoerodin@live.com>
2018-02-14 14:12:56 +0700

    add bootstrap style

---
+++ bs/index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/favicon.ico">
+
+    <title>Sticky Footer Template for Bootstrap</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="/bs/style.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <!-- Begin page content -->
+    <main role="main" class="container">
+      <article class="mt-5">
+        <header>
+          <h1>Sticky footer</h1>
+          <p class="text-muted lead-small"><time datetime="2009-10-10T19:10-08:00">09 Oct 2010</time></p>
+        </header>
+
+        <div class="lead">
+          <p>Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS.</p>
+          <p>Use <a href="../sticky-footer-navbar">the sticky footer with a fixed navbar</a> if need be, too.</p>
+          <blockquote class="blockquote">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS.</blockquote>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, quas? Magnam recusandae, delectus aut saepe obcaecati, ipsam itaque voluptate deleniti laboriosam dolor fuga facere voluptatum harum? Sed esse adipisci incidunt?.</p>
+        </div>
+      </article>
+    </main>
+
+    <footer class="footer">
+      <div class="container">
+        <span class="text-muted">Place sticky footer content here.</span>
+      </div>
+    </footer>
+  </body>
+</html>
